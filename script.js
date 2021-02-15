d8c7189b71548b7e872df5ca4c5e7cd6da1a1234
433MHz <krystian.izdebski1999@gmail.com>
2021-02-15 23:25:24 +0100

    Register popup implemented

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 var loginDiv = document.getElementById("loginDiv");
-
+var registerDiv = document.getElementById("registerDiv");
 
 function ShowFullLogin(){
-    loginDiv.style.width = "100%";
-    loginDiv.style.height = "30vh";
-    loginDiv.style.backgroundColor = "rgb(50, 146, 255)";
-    document.getElementById("loginButton").setAttribute("onclick","HideFullLogin()");
-    console.log(loginDiv);
+    HideFullRegister();
+    document.getElementById("loginDiv").innerHTML = BuildHTMLCode();
+    loginDiv.setAttribute("id", "loginDiv2");
+    function BuildHTMLCode(){
+        var HTMLCode = "";
+        HTMLCode = HTMLCode + "<input type=\"text\" placeholder=\"Login\"><br>";
+        HTMLCode = HTMLCode + "<input type=\"text\" placeholder=\"Hasło\"><br>";
+        HTMLCode = HTMLCode + "<input type=\"button\" value=\"Zaloguj\"><br>";
+        return HTMLCode;
+    }
 }
 
 function HideFullLogin(){
-    loginDiv.style.width = "";
-    loginDiv.style.height = "";
-    loginDiv.style.backgroundColor = "";
-    document.getElementById("loginButton").setAttribute("onclick","ShowFullLogin()");
+    loginDiv.setAttribute("id", "loginDiv");
+    loginDiv.setAttribute("onmouseout", "");
+    loginDiv.innerHTML = "<input id=\"loginButton\" type=\"button\" value=\"Zaloguj\" onclick=\"ShowFullLogin()\"><br>";
 }
 
 function ShowFullRegister(){
-
+    HideFullLogin();
+    document.getElementById("registerDiv").innerHTML = BuildHTMLCode();
+    registerDiv.setAttribute("id", "registerDiv2");
+    function BuildHTMLCode(){
+        var HTMLCode = "";
+        HTMLCode = HTMLCode + "<input type=\"text\" placeholder=\"Login\"><br>";
+        HTMLCode = HTMLCode + "<input type=\"text\" placeholder=\"Hasło\"><br>";
+        HTMLCode = HTMLCode + "<input type=\"text\" placeholder=\"Powtórz hasło\"><br>";
+        HTMLCode = HTMLCode + "<input type=\"button\" value=\"Zarejestruj\"><br>";
+        return HTMLCode;
+    }
 }
 
 function HideFullRegister(){
-
+    registerDiv.setAttribute("id", "registerDiv");
+    registerDiv.innerHTML = "<input id=\"registerButton\" type=\"button\" value=\"Zarejestruj\" onclick=\"ShowFullRegister()\"><br>";
 }
