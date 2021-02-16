f6f77ed1758650bab6e8e5d778ee49b821beba89
433MHz <krystian.izdebski1999@gmail.com>
2021-02-16 04:05:47 +0100

    Application created
    Downloaded from spring initializer

---
+++ TwiFac/src/main/java/pl/krystian/TwiFac/TwiFacApplication.java
@@ -0,0 +1,13 @@
+package pl.krystian.TwiFac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwiFacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwiFacApplication.class, args);
+	}
+
+}
