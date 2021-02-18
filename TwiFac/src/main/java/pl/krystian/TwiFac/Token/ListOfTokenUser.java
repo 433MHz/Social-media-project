48de178e88a384190cc2623a1da5845af92cac8b
Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-02-18 11:49:40 +0100

    Created branch with code for token operations

---
+++ TwiFac/src/main/java/pl/krystian/TwiFac/Token/ListOfTokenUser.java
@@ -0,0 +1,20 @@
+package pl.krystian.TwiFac.Token;
+
+import java.util.HashMap;
+
+public class ListOfTokenUser {
+	static HashMap<String, Integer> list;
+	
+	static void Add(String token, int id) {
+		list.put(token, id);
+	}
+	
+	static boolean IsTokeAvailable(String token) {
+		if(list.containsKey(token)) return false;
+		else return true;
+	}
+	
+	static int GetId(String token) {
+		return list.get(token);
+	}
+}
---
+++ TwiFac/src/main/java/pl/krystian/TwiFac/Token/TokenProceed.java
@@ -0,0 +1,7 @@
+package pl.krystian.TwiFac.Token;
+
+public class TokenProceed {
+	public int check(String token) {
+		return 0;
+	}
+}
