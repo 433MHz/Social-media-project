7b8a33f347369ee00e18ecee7987f94764d25fed
Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-02-18 18:21:41 +0100

    Added object class to inform about token authorization status

---
+++ TwiFac/src/main/java/pl/krystian/TwiFac/Token/TokenAuthenticationStatus.java
@@ -0,0 +1,23 @@
+package pl.krystian.TwiFac.Token;
+
+public class TokenAuthenticationStatus {
+
+	private String message;
+	private boolean isSuccessed;
+	
+	
+	
+	
+	public String getMessage() {
+		return message;
+	}
+	public boolean isSuccessed() {
+		return isSuccessed;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public void setSuccessed(boolean isSuccessed) {
+		this.isSuccessed = isSuccessed;
+	}
+}
